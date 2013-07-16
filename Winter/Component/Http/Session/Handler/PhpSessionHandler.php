7391131f2c2bac5971d62c59ed80ae85df6dd886
lorenzo <lorenzo@cl130>
2013-07-16 17:26:03 +0200

    Added basic support for native session

---
+++ Winter/Component/Http/Session/Handler/PhpSessionHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Winter\Component\Http\Session\Handler;
+/**
+ * Wrapper for native SessionHandler 
+ *
+ * @author Lorenzo Iannone
+ */
+class PhpSessionHandler extends \SessionHandler {
+    //put your code here
+}
+
+?>
---
+++ Winter/Component/Http/Session/Session.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Winter\Component\Http\Session;
+
+use Winter\Component\Http\Session\Interfaces\SessionInterface;
+
+/**
+ * Wrapper class for php session
+ *
+ * @author Lorenzo Iannone
+ */
+class Session implements SessionInterface{
+    
+    /** @var \SessionHandlerInterface  */
+    protected $saveHandler;
+    
+    
+    /**
+     * Default session constructor.
+     * Set the session handler.
+     * @param \SessionHandlerInterface $handler
+     */
+    public function __construct(\SessionHandlerInterface $handler) {
+        
+        //store the save handler
+        $this->saveHandler = $handler;
+        
+        //set the right session save handler 
+        if (version_compare(phpversion(), '5.4.0', '>=')) {
+            session_set_save_handler($this->saveHandler, false);
+        } else {
+            session_set_save_handler(
+                array($this->saveHandler, 'open'),
+                array($this->saveHandler, 'close'),
+                array($this->saveHandler, 'read'),
+                array($this->saveHandler, 'write'),
+                array($this->saveHandler, 'destroy'),
+                array($this->saveHandler, 'gc')
+            );
+        }
+
+    }
+    
+    
+    public function clear() {
+        $_SESSION = array();
+    }
+
+    public function get($key) {
+        return $_SESSION[$key];
+    }
+
+    public function getAll() {
+        return $_SESSION;
+    }
+
+    public function getId() {
+        return session_id();
+    }
+
+    public function getKeys() {
+        return array_keys($_SESSION);
+    }
+
+    public function hasKey($key) {
+        return array_key_exists($key, $_SESSION);
+    }
+
+    public function invalidate() {
+        unset ($_SESSION);
+    }
+
+    public function regenerateId() {
+        session_regenerate_id();
+    }
+
+    public function remove($key) {
+        unset ($_SESSION[$key]);
+    }
+
+    public function set($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    public function setId($id) {
+        session_id($id);
+    }
+
+    public function start() {
+        session_start();
+    }
+}
